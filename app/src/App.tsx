839898fe3fe906132b30ccda7a15ba215f81c921
Gilson Ramos <gilsrms0@gmail.com>
2025-09-26 10:59:52 -0300

    feat:estruturacao de pastas

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,12 @@
-
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './routes'
 
 
 export const App = () => {
-
-
   return (
-    <>
-      <p>gilson</p>
-    </>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,1 +1,15 @@
-export { };
+import React from 'react'
+import { Navigate, Routes, Route } from 'react-router-dom'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<p>pagina Inicial</p>} />
+    
+      <Route path="*" element={<Navigate to="/"/>} />
+    </Routes>
+  )
+
+}
+
+export default AppRoutes
