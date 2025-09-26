ec763b3535396a19aa80cc20f1d1627c6502d987
Gilson Ramos <gilsrms0@gmail.com>
2025-09-26 12:09:29 -0300

    feat:Adicionado react-router-dom

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@mui/material'
+import { LightTheme } from './shared/themes'
 import AppRoutes from './routes'
 
 
 export const App = () => {
   return (
+    <ThemeProvider theme={LightTheme}>
     <BrowserRouter>
       <AppRoutes/>
     </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
--- app/src/routes/index.tsx
+++ app/src/routes/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import { Navigate, Routes, Route } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<p>pagina Inicial</p>} />
+      <Route path="/" element={<Button variant='contained' color='primary'>Teste</Button>} />
     
       <Route path="*" element={<Navigate to="/"/>} />
     </Routes>
